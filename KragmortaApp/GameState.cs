53e08ae02d7773fce3b85842b4b6b2e6f59b0ebe
Alexander <allemeshov@gmail.com>
2021-12-17 21:21:00 +0300

    he-he

--- KragmortaApp/GameState.cs
+++ KragmortaApp/GameState.cs
@@ -33,7 +33,7 @@ namespace KragmortaApp
             Instance    = this;
             
             // Field = new GameField(10, 7);
-            Field = new GameField(7, 10);
+            Field = new GameField(2, 10);
 
             Profile = new Profile()
             {
